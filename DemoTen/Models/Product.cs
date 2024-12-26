54fef954259095359ed351a951336356f8dddeac
A-AlHirz <abdherz@hotmail.com>
2024-12-26 11:54:28 +0300

    Add project files.

---
+++ DemoTen/Models/Product.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//class  product  3  functions Name of products/ price and name/ name price quantity of the product & total
+namespace DemoTen.Models
+{
+    public class Product
+    {
+
+        public void GetInfo(string name)
+        {
+
+            Console.WriteLine($"Product Name: {name}");
+        }
+
+        public void GetInfo(string name, double price)
+        {
+            Console.WriteLine(($"Product Name: {name}, Price: {price}"));
+        }
+
+        public void GetInfo(string name, double price, int qty)
+        {
+            Console.WriteLine(($"Product Name: {name}, Price: {price}, Qty: {qty} , Total: {CalcTotal(qty,price)}"));
+        }
+        public int CalcTotal(int qty, double price)
+        {
+            return (int)(qty * price);
+        }
+
+        
+
+        //Add List of Products
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Qty { get; set; }
+
+        //Declare List of products
+        List<Product> products = new List<Product>();
+
+        public void AddProduct(string name, double price, int qty)
+        {
+            products.Add(new Product { Name = name, Price = price, Qty = qty });
+        }
+
+        public void DisplayProducts()
+        {
+            Console.WriteLine("-----------------Products-----------------");
+            foreach (Product item in products)
+            {
+                Console.WriteLine($"Product Name: {item.Name}, Price: {item.Price}, Qty: {item.Qty}, Total: {CalcTotal(item.Qty, item.Price)}");
+            }
+        }
+
+
+
+    }
+
+}
---
+++ DemoTen/Program.cs
@@ -0,0 +1,57 @@
+﻿using DemoTen.Models;
+#region Declaration
+//Object class
+
+Product product = new Product();
+
+string name;
+double price;
+int qty;
+#endregion
+
+#region AddProducts
+//Input
+Console.WriteLine("Enter product name");
+name = Console.ReadLine();
+
+Console.WriteLine("Enter product price");
+price = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter product quantity");
+qty = Convert.ToInt32(Console.ReadLine());
+#endregion
+
+#region Printing
+//Use Functions
+//Print name
+product.GetInfo(name);
+
+//Print name and price
+product.GetInfo(name, price);
+
+//Print name, price and quantity
+product.GetInfo(name, price, qty);
+
+#endregion
+
+
+#region Loop to display products
+Console.WriteLine("No. products: ");
+int count = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <=count; i++)
+{
+    Console.WriteLine("Enter product name");
+    string ProductName = Console.ReadLine();
+
+    Console.WriteLine("Enter product price");
+    double Price = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Enter product quantity");
+    int Qty = Convert.ToInt32(Console.ReadLine());
+
+    product.AddProduct(ProductName, Price, Qty);
+}
+product.DisplayProducts();
+
+#endregion
